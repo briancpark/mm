5df983b03b9ebd2f4cd9181a59211e48ace50c98
Brian Park <briancpark@berkeley.edu>
2021-07-05 18:44:23 +0000

    update cblas

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 COMPILER_FLAGS = -fopenmp -lmkl_intel_lp64 -lmkl_core -lmkl_gnu_thread -lpthread -lm -ldl
 
 cblas: cblas.cpp
-	g++ -L$(MKL_LIB_DIR) -I$(MKL_INCLUDE_DIR) cblas.cpp -o cblas $(COMPILER_FLAGS)
+	g++ -g -L$(MKL_LIB_DIR) -I$(MKL_INCLUDE_DIR) cblas.cpp -o cblas $(COMPILER_FLAGS)
 
 .PHONY : clean
 
--- cblas.cpp
+++ cblas.cpp
@@ -7,34 +7,44 @@
 using namespace std;
 using namespace std::chrono;
 
+/*
+Timing Intel's MKL Cblas function
+*/
+
 extern "C"
 {
    #include <mkl.h>
    #include <mkl_cblas.h>
 }
 
-int main() {
-    int n = 4096;
-    int rows = n;
-    int cols = n;
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        cout << "Missing argument n" << endl;
+        return -1;
+    }
+
+    char *pEnd;
+    unsigned long long int n = strtoull(argv[1], &pEnd, 10);
+    unsigned long long int rows = n;
+    unsigned long long int cols = n;
 
     double *A;
     double *B;
     double *C;
 
-    A = (double *) mkl_malloc(n * n * sizeof(double), 64);
-    B = (double *) mkl_malloc(n * n * sizeof(double), 64);
-    C = (double *) mkl_malloc(n * n * sizeof(double), 64);    
+    A = (double *) mkl_malloc(n * n * sizeof(double), 32768);
+    B = (double *) mkl_malloc(n * n * sizeof(double), 32768);
+    C = (double *) mkl_malloc(n * n * sizeof(double), 32768);    
     
-    for(int i=0; i<rows; i++) {
-        for(int j=0; j<cols; j++) {
-            A[i*cols+j] = (double) rand() / RAND_MAX * 2.0 - 1.0;
+    for (unsigned long long int i = 0; i < rows; i++) {
+        for (unsigned long long int j = 0; j < cols; j++) {
+            A[i * cols + j] = (double) rand() / RAND_MAX * 2.0 - 1.0;
         }
     }
 
-    for(int i=0; i<rows; i++) {
-        for(int j=0; j<cols; j++) {
-            B[i*cols+j] = (double) rand() / RAND_MAX * 2.0 - 1.0;
+    for(unsigned long long int i = 0; i < rows; i++) {
+        for(unsigned long long int j = 0; j < cols; j++) {
+            B[i * cols + j] = (double) rand() / RAND_MAX * 2.0 - 1.0;
         }
     }
 
@@ -43,9 +53,12 @@ int main() {
     auto end = high_resolution_clock::now();
 
     auto duration = duration_cast<microseconds>(end - start);
-  
-  
+
     cout << (double) duration.count() / 1000000 << endl;
+
+    mkl_free(A);
+    mkl_free(B);
+    mkl_free(C);
 
     return 0;
 }
